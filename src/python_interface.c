ac165d59b1d5f996021f6fdce287d87ccaa3adda
Patrick Naughton <patricknaughton01@gmail.com>
2022-11-29 21:46:22 -0600

    Created corresponding python file.
    Python side interface exposes `detect_primitives` interface
    that provides pointers to the outputs of the algorithm. Packed into
    `PrimitiveSet` object for convenience. This object should clean
    up memory when it is destructed, although it is possible to create
    a memory leak still. The memory it consumes can also be manually freed.

--- src/python_interface.c
+++ src/python_interface.c
@@ -4,30 +4,37 @@
 #include "polygon.h"
 #include "elsdc.h"
 
-int detect_primitives(Ring **ell_out, int *ell_count, Polygon **poly_out,
-  int *poly_count, int **out, double *in, size_t xsize, size_t ysize)
+extern int detect_primitives(Ring **ell_out, int **ell_labels, int *ell_count, 
+  Polygon **poly_out, int **poly_labels, int *poly_count, 
+  int **out, double *in, size_t xsize, size_t ysize)
 {
   PImageInt out_img;   /* output image having the same size as 'in'; the pixels
                           supporting a certain geometric primitive are marked
                           with the same label */
-  int *ell_labels=NULL;/* the pixels supporting a certain ellipse are marked
-                          with the same unique label */
-  Ring *ell_out = NULL;/* array containing the parameters of the detected
-                          ellipses; correlated with ell_labels, i.e. the i-th
-                          element of ell_labels is the label of the pixels
-                          supporting the ellipse defined by the parameters
-                          ell[i] */
-  int *poly_labels=NULL;/* the pixels supporting a certain polygon are marked
-                          with the same unique label */
-  Polygon *poly_out=NULL;/* array containing the parameters of the detected
-                          polygons; correlated with poly_labels, i.e. the i-th
-                          element of ell_labels is the label of the pixels
-                          supporting the polygon defined by the parameters
-                          poly[i] */
   struct ImageDouble in_img = {.data=in, .xsize=xsize, .ysize=ysize};
   out_img = new_PImageInt_ini(xsize, ysize, 0);
-  ELSDc(&in_img, ell_count, ell_out, &ell_labels, poly_count, poly_out,
-    &poly_labels, out_img);
+  ELSDc(&in_img, ell_count, ell_out, ell_labels, poly_count, poly_out,
+    poly_labels, out_img);
   *out = out_img->data;
+  free(out_img);
   return 0;
+}
+
+extern void free_outputs(Ring *ell_out, int *ell_labels, 
+  Polygon *poly_out, int *poly_labels, int poly_count, int *out)
+{
+  if(ell_out){
+    free(ell_out);
+    free(ell_labels);
+  }
+  if(poly_out){
+    for(int i = 0; i < poly_count; i++){
+      free(poly_out[i].pts);
+    }
+    free(poly_out);
+    free(poly_labels);
+  }
+  if(out){
+    free(out);
+  }
 }
